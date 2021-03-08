382508c12267ce70f882f5afb13d1fa23c03c3b4
Pranit Dhage <pranitdhage92@gmail.com>
2021-03-08 17:46:02 +0530

    Signin Functionality Of Backend Completed

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,31 +2,44 @@ const express = require('express')
 const db = require('../db')
 const config = require('../config')
 const utils = require('../utils')
+const crypto = require('crypto-js')
 
 const router = express.Router()
 
 //Signup
 router.post('/signup', (request, response) => {
   const { name, email, phone, password } = request.body // get sign up details from request
-  const statement = `insert into user(name, email, phone, password) values('${name}','${email}','${phone}','${password}')`
+  // Password Encryption
+  const encPwd = '' + crypto.SHA256(password)
+  const statement = `insert into user(name, email, phone, password) values('${name}','${email}','${phone}','${encPwd}')`
+  console.log(encPwd)
   db.execute(statement, (error, data) => {
     // result
     const result = utils.createResult(error, data)
-    if (!error) {
-      // mailer.sendEmail(
-      //   'signup.html',
-      //   'welcome to ecommerce application',
-      //   email,
-      //   (error, info) => {
-      //     response.send(result)
-      //   }
-      // )
-      response.send(result)
-      console.log("Signup Successful")
-    } else {
-      response.send(result)
-    }
 
+    // mailer.sendEmail(
+    //   'signup.html',
+    //   'welcome to ecommerce application',
+    //   email,
+    //   (error, info) => {
+    //     response.send(result)
+    //   }
+    // )
+    response.send(result)
+    console.log('Signup Successful')
+  })
+})
+
+//Signin
+router.post('/signin', (request, response) => {
+  const { email, password } = request.body
+  const encPwd = '' + crypto.SHA256(password)
+  const statement = `SELECT * FROM user where email = '${email}' AND password = '${encPwd}'`
+  db.execute(statement, (error, data) => {
+    // result
+    const result = utils.createResult(error, data)
+    response.send(result)
+    console.log(result)
   })
 })
 
