dfb94e9ac4f1a8d3f1154302018fa11113bef060
Pankaj Chaudhari <chaudharip371@gmail.com>
2021-03-14 10:23:31 +0530

    order reducer are added

---
+++ Frontend/src/reducers/orderReducer.js
@@ -0,0 +1,26 @@
+import {
+    ORDER_FETCH_REQUEST,
+    ORDER_FETCH_SUCCESS,
+    ORDER_FETCH_FAIL,
+    ORDER_FETCH_RESET
+  } from "../constants/orderConstants";
+
+  
+export const viewOrderReducer = (state = {}, action) => {
+    switch (action.type) {
+      case ORDER_FETCH_REQUEST:
+        return { loading: true };
+  
+      case ORDER_FETCH_SUCCESS:
+        return { loading: false, response: action.payload };
+  
+      case ORDER_FETCH_FAIL:
+        return { loading: false, error: action.payload };
+  
+      case ORDER_FETCH_RESET:
+        return {};
+  
+      default:
+        return state;
+    }
+  };
