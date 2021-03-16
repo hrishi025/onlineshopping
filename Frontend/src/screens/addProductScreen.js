92453a686a34455416e1ee2e0a4ae9dd5d11e87b
Pranit Dhage <pranitdhage92@gmail.com>
2021-03-16 23:20:48 +0530

    Add Product Screen Designed

--- Frontend/src/screens/addProductScreen.js
+++ Frontend/src/screens/addProductScreen.js
@@ -1,102 +1,129 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   addProduct,
   getAllCategories,
   getAllCompanies,
-} from "../actions/productActions";
-import { PRODUCT_POST_RESET } from "../constants/productConstants";
+} from '../actions/productActions'
+import { PRODUCT_POST_RESET } from '../constants/productConstants'
 
 const AddProductScreen = (props) => {
-  const addProductStore = useSelector((state) => state.addProductStore);
-  const { response, loading, error } = addProductStore;
+  const addProductStore = useSelector((state) => state.addProductStore)
+  const { response, loading, error } = addProductStore
 
-  const companyFetchStore = useSelector((state) => state.companyFetchStore);
+  const companyFetchStore = useSelector((state) => state.companyFetchStore)
 
-  const categoryFetchStore = useSelector((state) => state.categoryFetchStore);
+  const categoryFetchStore = useSelector((state) => state.categoryFetchStore)
 
-  const [company, setCompany] = useState("");
-  const [category, setCategory] = useState("");
-  const [prodtitle, setProdtitle] = useState("");
-  const [proddesc, setProddesc] = useState("");
-  const [prodprice, setProdprice] = useState("");
-  const [prodqty, setProdqty] = useState("");
+  const [company, setCompany] = useState('')
+  const [category, setCategory] = useState('')
+  const [prodtitle, setProdtitle] = useState('')
+  const [proddesc, setProddesc] = useState('')
+  const [prodprice, setProdprice] = useState('')
+  const [prodqty, setProdqty] = useState('')
   let [selectedFile, setSelectedFile] = useState([])
 
-  const categoryChangeHandler = (e) => setCategory(e.target.value);
-  const companyChangeHandler = (e) => setCompany(e.target.value);
+  const categoryChangeHandler = (e) => setCategory(e.target.value)
+  const companyChangeHandler = (e) => setCompany(e.target.value)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const addnewProduct = () => {
-    console.log("in add Product button function");
+    console.log('in add Product button function')
     dispatch(
-      addProduct(prodtitle, proddesc, category, prodprice, company, prodqty, selectedFile)
-    );
-  };
+      addProduct(
+        prodtitle,
+        proddesc,
+        category,
+        prodprice,
+        company,
+        prodqty,
+        selectedFile
+      )
+    )
+  }
 
   const onFileSelected = (event) => {
     setSelectedFile(event.target.files[0])
   }
 
   useEffect(() => {
-    dispatch(getAllCategories());
-    dispatch(getAllCompanies());
-  }, []);
+    dispatch(getAllCategories())
+    dispatch(getAllCompanies())
+  }, [])
 
   useEffect(() => {
-    if (response && response.status == "success") {
-      dispatch({ type: PRODUCT_POST_RESET });
-      props.history.push("/");
+    if (response && response.status == 'success') {
+      dispatch({ type: PRODUCT_POST_RESET })
+      props.history.push('/')
     } else if (error) {
       // there is an error while making the API call
-      dispatch({ type: PRODUCT_POST_RESET });
-      console.log(error);
-      alert("error while making API call");
+      dispatch({ type: PRODUCT_POST_RESET })
+      console.log(error)
+      alert('error while making API call')
     }
-  }, [response, loading, error]);
+  }, [response, loading, error])
 
   return (
-    <div>
-      <h1>Add Product Screen</h1>
+    <div className="signup-form">
+      <h2>Add New Product</h2>
+      <hr />
 
-      <div>
-        <label>Product Title</label>
+      <div className="form-group">
         <input
           type="text"
+          background-color="#3CBC8D"
+          color="white"
+          className="form-control"
+          name="Prod_title"
+          placeholder="Product Title"
+          required="required"
           onChange={(e) => setProdtitle(e.target.value)}
-        ></input>
+        />
       </div>
 
-      <div>
-        <label>Product Description</label>
+      <div className="form-group">
         <input
           type="text"
-          onChange={(e) => setProddesc(e.target.value)}
-        ></input>
+          className="form-control"
+          name="prod_desc"
+          placeholder="Product Description"
+          required="required"
+          onChange={(e) => setProddesc(e.target.value)}></input>
       </div>
 
-      <div>
-        <label>Product Price</label>
+      <div className="form-group">
         <input
-          type="number"
-          onChange={(e) => setProdprice(e.target.value)}
-        ></input>
+          type="text"
+          className="form-control"
+          name="prod_price"
+          placeholder="Product Price"
+          required="required"
+          onChange={(e) => setProdprice(e.target.value)}></input>
       </div>
 
-      <div>
-        <label>Product Quantity</label>
+      <div className="form-group">
         <input
-          type="number"
-          onChange={(e) => setProdqty(e.target.value)}
-        ></input>
+          type="text"
+          className="form-control"
+          name="prod_qty"
+          placeholder="Product Quantity"
+          required="required"
+          onChange={(e) => setProdqty(e.target.value)}></input>
       </div>
 
-      <div>
-        <label>Product Category</label>
+      <div className="form-group">
+        <label style={{ float: 'left' }}>
+          <h6>Choose Category</h6>
+        </label>
         {console.log(categoryFetchStore.response)}
-        <select id="catTitle" size="0" onChange={categoryChangeHandler}>
+        <select
+          id="catTitle"
+          class="custom-select custom-select-sm"
+          size="0"
+          required="required"
+          onChange={categoryChangeHandler}>
           <option value="default">Choose</option>
           {categoryFetchStore.response &&
             categoryFetchStore.response.data &&
@@ -105,19 +132,25 @@ const AddProductScreen = (props) => {
                 <option
                   key={cat.cat_id}
                   value={cat.cat_id}
-                  onChange={categoryChangeHandler}
-                >
+                  onChange={categoryChangeHandler}>
                   {cat.cat_title}
                 </option>
-              );
+              )
             })}
         </select>
       </div>
 
-      <div>
-        <label>Product Company</label>
+      <div className="form-group">
+        <label style={{ float: 'left' }}>
+          <h6>Choose Company</h6>
+        </label>
         {console.log(companyFetchStore.response)}
-        <select id="compTitle" size="0" onChange={companyChangeHandler}>
+        <select
+          id="compTitle"
+          class="custom-select custom-select-sm"
+          size="0"
+          required="required"
+          onChange={companyChangeHandler}>
           <option value="default">Choose</option>
           {companyFetchStore.response &&
             companyFetchStore.response.data &&
@@ -126,27 +159,39 @@ const AddProductScreen = (props) => {
                 <option
                   key={comp.comp_id}
                   value={comp.comp_id}
-                  onChange={categoryChangeHandler}
-                >
+                  onChange={categoryChangeHandler}>
                   {comp.comp_title}
                 </option>
-              );
+              )
             })}
         </select>
       </div>
 
-      <div>
-        <label>Photo</label>
-        <input onChange={onFileSelected} type='file' />
+      <div className="form-group">
+        <label style={{ float: 'left' }}>
+          <h6>Choose Photo</h6>
+        </label>
+
+        <input
+          type="file"
+          className="form-control"
+          name="prod_photo"
+          placeholder="Product Photo"
+          required="required"
+          onChange={onFileSelected}
+        />
       </div>
 
-      <div>
-        <button type="submit" onClick={addnewProduct}>
-          addProduct
+      <div className="form-group">
+        <button
+          type="submit"
+          onClick={addnewProduct}
+          className="btn btn-primary btn-block btn-lg">
+          Save Product
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AddProductScreen;
+export default AddProductScreen
