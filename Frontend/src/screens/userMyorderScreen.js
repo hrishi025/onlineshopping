06532fe75c6d25d782f2de7905b5993562b1057d
Pranit Dhage <pranitdhage92@gmail.com>
2021-03-18 12:28:38 +0530

    User My Order Page Design

--- Frontend/src/screens/userMyorderScreen.js
+++ Frontend/src/screens/userMyorderScreen.js
@@ -27,49 +27,68 @@ const UserMyOrderScreen = (props) => {
   }, [])
 
   return (
-    <div>
-      My Orders Screen
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>MyOrder ID</th>
-            <th>Order Date</th>
-            <th>Order Status</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {viewMyOrderStore.response &&
-            viewMyOrderStore.response.data &&
-            viewMyOrderStore.response.data.length > 0 &&
-            viewMyOrderStore.response.data.map((p) => {
-              return (
-                <tr>
-                  <td>{p.myorder_id}</td>
-                  <td>{p.orderDate}</td>
-                  <td>{p.status}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        onCancelOrder(p)
-                      }}
-                      className="btn btn-sm btn-danger">
-                      Cancel
-                    </button>
-
-                    <button
-                      onClick={() => {
-                        onOrderDetails(p)
-                      }}
-                      className="btn btn-sm btn-success">
-                      View Order Details
-                    </button>
-                  </td>
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
+    <div className="container">
+      <div id="wrapper">
+        <div className="d-flex flex-column" id="content-wrapper">
+          <div id="content">
+            <div className="container-fluid">
+              <div className="card shadow">
+                <div className="card-header py-3">
+                  <p className="text-primary m-0 fw-bold">User Order List</p>
+                </div>
+                <div className="card-body">
+                  <div
+                    className="table-responsive table mt-2"
+                    id="dataTable"
+                    role="grid"
+                    aria-describedby="dataTable_info">
+                    <table className="table my-0" id="dataTable">
+                      <thead>
+                        <tr>
+                          <th>Order Date</th>
+                          <th>Order Status</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {viewMyOrderStore.response &&
+                          viewMyOrderStore.response.data &&
+                          viewMyOrderStore.response.data.length > 0 &&
+                          viewMyOrderStore.response.data.map((p) => {
+                            return (
+                              <tr>
+                                <td>{p.orderDate}</td>
+                                <td>{p.status}</td>
+                                <td>
+                                  {p.status == 'not delivered' && (
+                                    <button
+                                      onClick={() => {
+                                        onCancelOrder(p)
+                                      }}
+                                      className="btn btn-sm btn-danger mx-2">
+                                      Cancel
+                                    </button>
+                                  )}
+                                  <button
+                                    onClick={() => {
+                                      onOrderDetails(p)
+                                    }}
+                                    className="btn btn-sm btn-success mx-2">
+                                    Order Details
+                                  </button>
+                                </td>
+                              </tr>
+                            )
+                          })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
